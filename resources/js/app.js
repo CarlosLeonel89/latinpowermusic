757de97f0cc51411ed9223738330b8283fce00bd
Carlos Leonel <carlos.najera.is@unipolidgo.edu.mx>
2024-03-21 03:18:56 -0600

    Modification in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from 'ziggy-js';
 
+// app.js
 const postgres = require('postgres');
 require('dotenv').config();
 
